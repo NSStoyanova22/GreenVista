94179ee43695b90e1a8b45dbb25d65f495304219
nsstoyanova22 <nsstoyanova22@codingburgas.bg>
2024-02-19 12:21:47 +0200

    Fix problems

--- src/main.jsx
+++ src/main.jsx
@@ -13,9 +13,7 @@ import Home from "./pages/Home"
 import ErrorPage from "./error-page";
 import { Register } from './pages/Register';
 import { Forum } from './pages/Forum.jsx';
-import { Profile } from './pages/Profile';
 import { YourProfile } from './pages/YourProfile';
-import { SendMessage } from './pages/SendMessage';
 
 import '../styles/app.css'
 
@@ -28,9 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path={"/login"} element={<LogIn />} errorElement={<ErrorPage />}/>
           <Route path={"/register"} element={<Register />} errorElement={<ErrorPage />}/>
           <Route path={"/forum"} element={<Forum />} errorElement={<ErrorPage />}/>
-          <Route path={"/profile"} element={<Profile />} errorElement={<ErrorPage />}/>
           <Route path={"/yourprofile"} element={<YourProfile />} errorElement={<ErrorPage />}/>
-          <Route path={"/sendmessage"} element={<SendMessage />} errorElement={<ErrorPage />} />
         </Routes>
     </BrowserRouter>
     
